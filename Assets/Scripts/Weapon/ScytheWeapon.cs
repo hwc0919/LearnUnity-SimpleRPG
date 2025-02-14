bb4d94a713b9b6a0d7d5689cf3737a9aacf3be6f
He, Wanchen <hwc14@qq.com>
2025-02-14 17:03:21 +0800

    attack.

---
+++ Assets/Scripts/Weapon/ScytheWeapon.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScytheWeapon : Weapon
+{
+    public const string ANIMATION_PARAM_ISATTACKING = "isAttacking";
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public override void Attack()
+    {
+        animator.SetTrigger(ANIMATION_PARAM_ISATTACKING);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            print("Trigger with enemy " + other.name);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+}
