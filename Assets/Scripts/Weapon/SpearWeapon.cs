52e1d09e47cf5b52b5b9d302ba8012dd0f6f88c3
He, Wanchen <hwc14@qq.com>
2025-02-18 15:36:51 +0800

    attack and reload.

--- Assets/Scripts/Weapon/SpearWeapon.cs
+++ Assets/Scripts/Weapon/SpearWeapon.cs
@@ -1,26 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class SpearWeapon : Weapon
 {
     public GameObject bulletPrefab;
+    public float bulletSpeed = 10f;
+    private GameObject bulletInHand;
 
     public override void Attack()
     {
-        GameObject bullet = GameObject.Instantiate(bulletPrefab, transform.position, transform.rotation);
-        bullet.GetComponent<Rigidbody>().velocity = transform.forward;
+        if (!bulletInHand)
+        {
+            print("No bullet");
+            return;
+        }
+        // fire
+        // Animator animator = bulletInHand.GetComponent<Animator>();
+        // animator.SetTrigger("ready");
+        // bulletInHand.transform.SetParent(null);
+        print("forward: " + transform.forward);
+        bulletInHand.transform.SetParent(null);
+        bulletInHand.GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed;
+        print("velocity: " + bulletInHand.GetComponent<Rigidbody>().velocity);
+
+        bulletInHand = null;
+        Invoke("NewBulletInHand", 2.0f);
+    }
+
+    void NewBulletInHand()
+    {
+        if (bulletInHand)
+        {
+            throw new Exception("Already exists a bullet");
+        }
+        print("New bullet");
+        bulletInHand = Instantiate(bulletPrefab, transform); // create as child
+        bulletInHand.SetActive(true);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        NewBulletInHand();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
